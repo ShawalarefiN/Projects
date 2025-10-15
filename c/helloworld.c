ab8ea43a94942b6b9bd184d92e5e43c3fdb8efe2
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-16 03:12:32 +0600

    first project files created

---
+++ c/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    int num1 = 1;
+    int num2 = 2;
+
+    int result = num1 + num2;
+    printf("%d\n", result);
+    return 0;
+}
---
+++ js/calculator.js
@@ -0,0 +1,5 @@
+number1 = 5;
+number2 = 10;
+sum = number1 + number2;
+
+console.log(sum);
